9178b02213f2b24a77d5ecaf170f6f0d890b276c
alife-echo <alifesilva90@gmail.com>
2023-02-23 21:11:41 -0300

    mustahce aulas

--- mustache/src/routers/index.ts
+++ mustache/src/routers/index.ts
@@ -4,7 +4,26 @@ import {Request,Response,Router} from "express";
 const router = Router()
 
 router.get('/',(req:Request,res:Response)=>{
-     res.render('home')
+    let age : number = 17
+    let showOld : boolean = false
+
+    if(age > 18){
+        showOld = true
+    }
+    else {
+        showOld = false
+    }
+
+    let user = {
+         name:'Álife',
+         lastName:'moraes',
+         showWelcome:true,
+         showOld
+    }
+     res.render('home',{
+         user
+
+     })
 })
 
 router.get('/cadastro',(req:Request,res:Response)=>{
