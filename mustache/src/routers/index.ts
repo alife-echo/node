650bfa2ce74837caca8d054d653d7f1790e6e9d0
alife-echo <alifesilva90@gmail.com>
2023-03-04 20:54:36 -0300

    post

--- mustache/src/routers/index.ts
+++ mustache/src/routers/index.ts
@@ -48,10 +48,34 @@ router.get('/nome',(req:Request,res:Response)=>{
     console.log("query string",req.query)  
     let nome : string = req.query.nome as string
     let sobrenome : string = req.query.sobrenome as string
+    let idade : string = req.query.idade as string
     res.render('pages/nome',{
          nome,
-         sobrenome
+         sobrenome,
+         idade
     })
 
 })
+// Erro: Variable 'x' is used before being assigned. usando o compilador está alertando que você está tentando acessar o valor de uma variável que ainda não foi inicializada com um valor.
+router.get('/idade',(req:Request,res:Response)=>{
+        res.render('pages/idade')
+})
+
+router.post('/idade-resultado',(req:Request,res:Response)=>{
+    let idade : number = 0
+    let showAge : boolean = false
+    if(req.body.ano){
+        let anoNascimento : number = parseInt(req.body.ano as string) 
+        let anoAtual : number = new Date().getFullYear()
+        idade = anoAtual - anoNascimento
+        showAge = true
+    }
+    
+     res.render('pages/idade',{
+         idade,
+         showAge
+     })
+})
+
+
 export default router
--- mustache/src/server.ts
+++ mustache/src/server.ts
@@ -2,11 +2,15 @@ import express,{Request,Response} from 'express'
 import mustache from 'mustache-express'
 import routerMains from './routers/index'
 import path from 'path'
-
 const server = express()
 server.set('view engine','mustache')
 server.set('views',path.join(__dirname,'./views'))
 server.engine('mustache',mustache())
+
+server.use(express.urlencoded({extended:true})) // pegar os dados do corpo da requisição dentro da rota
+//extend:true --> mostra os dados detalhadamente
+//POST --> mandar os dados internamente
+
 server.use(routerMains)
 
 server.use((req:Request,res:Response)=>{
