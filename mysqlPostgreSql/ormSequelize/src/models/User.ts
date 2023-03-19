7d6a76763a26d586a3e763aa33c03f92f0149608
alife-echo <alifesilva90@gmail.com>
2023-03-19 19:17:57 -0300

    anotações banco

---
+++ mysqlPostgreSql/ormSequelize/src/models/User.ts
@@ -0,0 +1,29 @@
+import {Model,DataTypes} from 'sequelize'
+import {sequelize} from '../instances/mysql'
+
+
+interface UserInstance extends Model{
+    id:number,
+    name:string,
+    age:number
+}
+
+export const User = sequelize.define<UserInstance>("User",{
+    id:{
+        primaryKey:true,
+        type:DataTypes.INTEGER,
+    },
+    nameUser:{
+        type:DataTypes.STRING
+    },
+    ageUser:{
+        type:DataTypes.INTEGER,
+        defaultValue:18
+    }
+},{
+    tableName:'users',
+    timestamps:false
+})
+
+//createdAt
+//updatedAt
--- mysqlPostgreSql/ormSequelize/src/server.ts
+++ mysqlPostgreSql/ormSequelize/src/server.ts
@@ -3,7 +3,7 @@ import path from 'path'
 import mustache from 'mustache-express'
 import dotenv from 'dotenv'
 //import {sequelize} from '../src/instances/mysql'
-import {sequelize} from '../src/instances/pg'
+import { User } from './models/User'
 const server = express()
 dotenv.config()
 server.set('view engine','mustache')
@@ -13,15 +13,16 @@ server.use(express.static(path.join(__dirname,'../public')))
 server.use(express.urlencoded({extended:true}))
 
 server.get('/', async (req:Request,res:Response)=>{
-     
-    try{
-      await sequelize.authenticate()
-      console.log('Conexão estabelecida com sucesso')
-      res.send('<h1> olá menor </h1>')
-    }
-    catch (error){
-        console.log('Deu error',error)
-    }
+  try {
+    let users = await User.findAll() 
+    
+    res.render('pages/home',{
+       users
+    })
+  }
+  catch(error){
+    console.log(error)
+  }
   
 })
 
