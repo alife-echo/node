d0095594284d2a6798f4040348fdd68e5e257c5e
alife-echo <alifesilva90@gmail.com>
2023-03-21 19:55:53 -0300

    tipos de consulta

--- mysqlPostgreSql/ormSequelize/src/server.ts
+++ mysqlPostgreSql/ormSequelize/src/server.ts
@@ -4,6 +4,7 @@ import mustache from 'mustache-express'
 import dotenv from 'dotenv'
 //import {sequelize} from '../src/instances/mysql'
 import { User } from './models/User'
+import { Op } from 'sequelize'
 const server = express()
 dotenv.config()
 server.set('view engine','mustache')
@@ -14,8 +15,25 @@ server.use(express.urlencoded({extended:true}))
 
 server.get('/', async (req:Request,res:Response)=>{
   try {
-    let users = await User.findAll() 
-    
+    let users = await User.findAll({
+      // attributes:['nameUser','ageUser'],--> pegar colunas especificas
+      attributes:{exclude:['id']}, // --> não pegar a coluna,
+      //where:{nameUser:'Paulo',ageUser:55} // --> condicional quero apenas a linha com o nome usuario Paulo e que tenha 55 anos
+      where:{
+        [Op.or]:[
+            {ageUser:55},
+            {ageUser:30}
+            //{nameUser:'Paulo'}
+        ],
+        /*
+        ageUser:{ 
+          [Op.or]:[30,55]  --> outra forma
+        }
+        */
+       
+      }
+    }) 
+    console.log(users)
     res.render('pages/home',{
        users
     })
