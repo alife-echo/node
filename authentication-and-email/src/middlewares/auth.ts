555761e735f17a70c403171833dc452a3ed5bc87
alife-echo <alifesilva90@gmail.com>
2023-07-03 15:52:50 -0300

    anotations auth basic 64

--- authentication-and-email/src/middlewares/auth.ts
+++ authentication-and-email/src/middlewares/auth.ts
@@ -4,25 +4,24 @@ export const Auth = {
     private: async (req:Request,res:Response,next:NextFunction) =>{
         // Fazer verificação de auth
         let success = false
-        console.log(req.headers.authorization)
-        if(req.headers.authorization){
-            let hash : string = req.headers.authorization.substring(6)
-            let decoded : string = Buffer.from(hash,'base64').toString()
-            let data : string[]  = decoded.split(':')
-            if(data.length  ===  2){
+        if(req.headers.authorization){ // se o cabeçalho da requisição possuir um email e senha em base64
+            let hash : string = req.headers.authorization.substring(6) // separe a string 'Basic' do hash
+            let decoded : string = Buffer.from(hash,'base64').toString() // decodifique o base64 e transforme em string
+            let data : string[]  = decoded.split(':') //decode ira retornar 'email:senha' usando o splite ficara ['email','senha']
+            if(data.length  ===  2){ // se a requisição possuir email e senha
                let hasUser = await User.findOne({
                  where:{
-                    email:data[0],
-                    password:data[1]
+                    email:data[0],   // procure no banco de dados 
+                    password:data[1]  // procure no banco de dados 
                  }
                })
-               if(hasUser){
-                success = true
+               if(hasUser){ // se o usuario estiver no banco
+                success = true // troque o sucess para true
                }
             }
         }
-        if(success){
-            next()
+        if(success){ // se success for true
+            next() //conceda a passagem para o endpoit list
         }
         else{
             res.status(403) // Não permitido
