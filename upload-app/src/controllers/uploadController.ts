aef04f1e5f5604160596a6e09b512d5abd946196
alife-echo <alifesilva90@gmail.com>
2023-08-09 21:29:33 -0300

    types storages

--- upload-app/src/controllers/uploadController.ts
+++ upload-app/src/controllers/uploadController.ts
@@ -1,9 +1,12 @@
 import { Request,Response} from "express";
 
 export const uploadFile = async (req:Request,res:Response)=>{
+    /*
     const files  = req.files as {[fieldname:string]:Express.Multer.File[]}
     console.log('AVATAR',files.avatar)
     console.log('GALLERY',files.gallery)
-
+    */
+    console.log('FILE',req.file)
+    console.log('FILES',req.files)
     res.json({})
 }
--- upload-app/src/routers/routerMain.ts
+++ upload-app/src/routers/routerMain.ts
@@ -1,15 +1,26 @@
 import { Router } from "express";
-import multer from "multer";
+import multer, { FileFilterCallback } from "multer";
 import * as uploadController from '../controllers/uploadController'
-
+import express,{Request} from 'express'
 
 const router = Router()
-const upload = multer({dest:'./tmp'})
-
+const storageConfig = multer.diskStorage({ // diskStorage = vou armenar no disco de armazenamento da maquina
+      destination:(req:Request,file:Express.Multer.File,cb)=>{ // responsavel para destino de armazenamento do arquivo
+               cb(null,'./tmp')
+      }, 
+      filename:(req:Request,file:Express.Multer.File,cb)=>{
+          let randomName = Math.floor(Math.random() * 99999) 
+          cb(null,`${randomName + Date.now()}.jpg`)
+      }
+})
+const upload = multer({
+     storage:storageConfig
+    })
+/*
 router.post('/upload', upload.fields([
      {name:'avatar',maxCount:1},
      {name:'gallery',maxCount:3}
 ]),uploadController.uploadFile)
-
-
+*/
+router.post('/upload',upload.single('avatar'),uploadController.uploadFile)
 export default router
