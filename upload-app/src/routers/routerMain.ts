3058c20590bb593c69ec142d6933990b26c98129
alife-echo <alifesilva90@gmail.com>
2023-08-14 10:56:06 -0300

    validator archives

--- upload-app/src/routers/routerMain.ts
+++ upload-app/src/routers/routerMain.ts
@@ -4,6 +4,7 @@ import * as uploadController from '../controllers/uploadController'
 import express,{Request} from 'express'
 
 const router = Router()
+/*
 const storageConfig = multer.diskStorage({ // diskStorage = vou armenar no disco de armazenamento da maquina
       destination:(req:Request,file:Express.Multer.File,cb)=>{ // responsavel para destino de armazenamento do arquivo
                cb(null,'./tmp')
@@ -12,9 +13,17 @@ const storageConfig = multer.diskStorage({ // diskStorage = vou armenar no disco
           let randomName = Math.floor(Math.random() * 99999) 
           cb(null,`${randomName + Date.now()}.jpg`)
       }
-})
+})*/
 const upload = multer({
-     storage:storageConfig
+     dest:'./tmp',
+     fileFilter:(req,file,cb)=>{
+         // cb(null,false) ->  não aceita nada que for enviado
+         //cb(null,true)  -> aceita tudo que for enviado
+         const allowed:string[] = ['image/jpg','image/jpeg','image/png'] // -> especifico os tipos de imagem para validação
+         cb(null,allowed.includes(file.mimetype)) // --> o arquivo sera liberado caso o arquivo esteja incluso nos tipos definidos
+        },
+        limits:{fieldSize:20000000} // -> especificar o tamanho do aceitavel para do arquivo 20mb
+        
     })
 /*
 router.post('/upload', upload.fields([
