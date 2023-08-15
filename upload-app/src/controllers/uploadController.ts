c85c7424e07f564ba60d6eccdae9f5bd672efd06
alife-echo <alifesilva90@gmail.com>
2023-08-15 16:00:45 -0300

    unlink img fs promise

--- upload-app/src/controllers/uploadController.ts
+++ upload-app/src/controllers/uploadController.ts
@@ -1,5 +1,6 @@
 import { Request,Response} from "express";
 import sharp from "sharp";
+import {unlink} from 'fs/promises'
 export const uploadFile = async (req:Request,res:Response)=>{
     /*
     const files  = req.files as {[fieldname:string]:Express.Multer.File[]}
@@ -9,11 +10,13 @@ export const uploadFile = async (req:Request,res:Response)=>{
     console.log('FILE',req.file)
     console.log('FILES',req.files)*/
     if(req.file){
+        const filename : string = `${req.file.filename}.jpg`
         await sharp(req.file.path).resize(300,300,{
              fit:sharp.fit.cover,
              position:'center'
-        }).toFormat('jpeg').toFile(`./public/media/${req.file.filename}.jpg`)
-        res.json({image:`${req.file.filename}.jpg`})
+        }).toFormat('jpeg').toFile(`./public/media/${filename}`)
+        await unlink(req.file.path)
+        res.json({image:`${filename}`})
     }else{
         res.status(400).json({error:'Arquivo inválido'})
     }
